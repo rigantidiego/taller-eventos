79bece634cc8ccc9dd904cfc0e841d855f134a36
rigantidiego <rigantidiego@gmail.com>
2023-09-01 22:37:34 -0300

    Solucion click

--- evento.js
+++ evento.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     const div = document.getElementById('div');
+
     div.addEventListener("click", () => {
         alert('Hola! Soy el div!');
+    });
+
+    const boton = document.getElementById('boton');
+    boton.addEventListener("click", function(event) {
+        alert('Hola!');
+        event.stopPropagation(); // Detener la propagación del evento si es necesario
     });
 });
